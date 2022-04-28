34595532b1e44eb3c62dcce2099ac208977a0fb6
mthiagoalves <m.thiago.ales@live.com>
2022-04-27 22:37:50 -0300

    dotenv configs

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const route = require('./src/routes/route');
 const connectToDb = require('./src/database/database');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 connectToDb();
 
@@ -13,5 +14,5 @@ app.use(cors());
 app.use('/books', route);
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta http://localhost:${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDb = () => {
   mongoose
-    .connect('mongodb://localhost:27017/books-db', {
+    .connect(process.env.URI_DATABASE, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
